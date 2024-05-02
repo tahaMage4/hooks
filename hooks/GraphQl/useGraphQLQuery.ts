ab4dd98697217db2eeedeb11ccbab629d56857d4
tahaMage4 <taha@mage4.com>
2024-05-02 19:38:52 +0500

    no chnage

--- hooks/GraphQl/useGraphQLQuery.ts
+++ hooks/GraphQl/useGraphQLQuery.ts
@@ -1,29 +1,29 @@
-// import { useEffect } from "react";
-// import { useQuery, gql } from "@apollo/client";
+import { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
 
-// // Define your GraphQL query here
-// const YOUR_GRAPHQL_QUERY = gql`
-//   query YourGraphQLQuery {
-//     // Your query definition goes here
-//   }
-// `;
+// Define your GraphQL query here
+const YOUR_GRAPHQL_QUERY = gql`
+  query YourGraphQLQuery {
+    // Your query definition goes here
+  }
+`;
 
-// // Custom hook for executing GraphQL queries
-// const useGraphQLQuery = () => {
-// 	// Fetch data using the Apollo useQuery hook
-// 	const { data, loading, error } = useQuery(YOUR_GRAPHQL_QUERY);
+// Custom hook for executing GraphQL queries
+const useGraphQLQuery = () => {
+	// Fetch data using the Apollo useQuery hook
+	const { data, loading, error } = useQuery(YOUR_GRAPHQL_QUERY);
 
-// 	// Perform any additional logic here if needed
-// 	// For example, you can map or transform the data
+	// Perform any additional logic here if needed
+	// For example, you can map or transform the data
 
-// 	// useEffect to trigger the query when the component mounts
-// 	useEffect(() => {
-// 		// Trigger the query here if needed
-// 		// For example, you can call the refetch method to manually trigger the query
-// 		// The query will also be executed automatically by useQuery
-// 	}, []);
+	// useEffect to trigger the query when the component mounts
+	useEffect(() => {
+		// Trigger the query here if needed
+		// For example, you can call the refetch method to manually trigger the query
+		// The query will also be executed automatically by useQuery
+	}, []);
 
-// 	return { data, loading, error };
-// };
+	return { data, loading, error };
+};
 
-// export default useGraphQLQuery;
+export default useGraphQLQuery;
